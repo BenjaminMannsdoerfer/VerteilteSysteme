01cb3ab3a483359a185421a628e8a5d1c84dd1ad
Benjamin Mannsdörfer <benjamin.mannsdoerfer@htwg-konstanz.de>
2022-03-24 19:21:50 +0100

    added task2

--- Aufgabe1/src/aqua/blatt1/broker/Broker.java
+++ Aufgabe1/src/aqua/blatt1/broker/Broker.java
@@ -8,11 +8,23 @@ import aqua.blatt1.common.msgtypes.RegisterRequest;
 import aqua.blatt1.common.msgtypes.RegisterResponse;
 import messaging.Endpoint;
 import messaging.Message;
+
+import javax.swing.*;
+import java.io.Serializable;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Broker {
-    private Endpoint endpoint;
-    private ClientCollection<InetSocketAddress> clientCollection;
+    private boolean running = true;
+    private final Endpoint endpoint;
+    private final ClientCollection<InetSocketAddress> clientCollection;
+    private static final int NUM_THREADS = 5;
+    private final ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
 
     private Broker() {
         this.endpoint = new Endpoint(Properties.PORT);
@@ -24,24 +36,43 @@ public class Broker {
         broker.broker();
     }
 
-    public void broker() {
-        boolean running = true;
-        while (running) {
-            Message message = endpoint.blockingReceive();
-            System.out.println(message.getPayload());
-            if (message.getPayload() instanceof RegisterRequest) {
-                register(message.getSender());
-            } else if (message.getPayload() instanceof DeregisterRequest) {
-                deregister(message.getSender());
-            } else if (message.getPayload() instanceof HandoffRequest) {
-                handoffFish(((HandoffRequest) message.getPayload()), message.getSender());
-            } else {
-                running = false;
+    private class BrokerTask implements Runnable {
+        ClientCollection<InetSocketAddress> clientCollection;
+        Serializable payload;
+        InetSocketAddress sender;
+
+        private BrokerTask(Serializable payload, InetSocketAddress sender, ClientCollection<InetSocketAddress> clientCollection) {
+            this.clientCollection = clientCollection;
+            this.payload = payload;
+            this.sender = sender;
+        }
+
+        @Override
+        public void run() {
+            if (payload instanceof RegisterRequest) {
+                register(sender);
+            } else if (payload instanceof DeregisterRequest) {
+                deregister(sender);
+            } else if (payload instanceof HandoffRequest) {
+                handoffFish(((HandoffRequest) payload), sender);
             }
         }
     }
 
-    private void register(InetSocketAddress address) {
+    // dispatcher
+    public void broker() {
+        executor.execute(() -> {
+            JOptionPane.showMessageDialog(null, "Press ->OK<- when you want to stop");
+            running = false;
+        });
+        while (running) {
+            Message message = endpoint.blockingReceive();
+            executor.execute(new BrokerTask(message.getPayload(), message.getSender(), clientCollection));
+        }
+        executor.shutdown();
+    }
+
+    private synchronized void register(InetSocketAddress address) {
         String id = "tank" + clientCollection.size();
         System.out.println("register " + id);
         clientCollection.add(id, address);
@@ -49,14 +80,15 @@ public class Broker {
         endpoint.send(address, new RegisterResponse(id));
     }
 
-    private void deregister(InetSocketAddress address) {
+    private synchronized void deregister(InetSocketAddress address) {
         System.out.println("deregister " + clientCollection.indexOf(address));
         clientCollection.remove(clientCollection.indexOf(address));
     }
 
-    private void handoffFish(HandoffRequest handoffRequest, InetSocketAddress address) {
+    private synchronized void handoffFish(HandoffRequest handoffRequest, InetSocketAddress address) {
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+        lock.writeLock().lock();
         InetSocketAddress neighbor;
-        // handoffRequest.getFish().hitsEdge()
         if (handoffRequest.getFish().getDirection() == Direction.LEFT && handoffRequest.getFish().getX() == 0) {
             neighbor = clientCollection.getLeftNeighorOf(clientCollection.indexOf(address));
         } else {
@@ -65,5 +97,6 @@ public class Broker {
         System.out.println("Fish entered");
         System.out.println(clientCollection.getClient(clientCollection.indexOf(address)));
         endpoint.send(neighbor, handoffRequest);
+        lock.writeLock().unlock();
     }
 }
